5e94f3c55080556b9c49a09ae3b026c78fe78655
Christian Frichot <xntrik@gmail.com>
2012-04-22 14:01:05 +0800

    Default Home controller in place, and we are using CloudFront for the PROD CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Ssa::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://d3eixeq2ozvage.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
