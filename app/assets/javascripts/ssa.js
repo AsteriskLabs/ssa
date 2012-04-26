42b2e9a31cb0660a7b064f83990734473c96fd96
Christian Frichot <xntrik@gmail.com>
2012-04-26 10:14:41 +0800

    More work on the UI

--- app/assets/javascripts/ssa.js
+++ app/assets/javascripts/ssa.js
@@ -2,11 +2,15 @@ var JTable = function() {};
 JTable.Setup = function() {
     var table = $('.jtable');
     $('caption', table).addClass('ui-state-default');
+    $('caption', table).css('font-size','20px');
+    $('caption', table).css('padding','10px 0 10px');
     $('th', table).addClass('ui-state-default');
+    $('th', table).css('padding','5px 0 5px 5px');
     $('td', table).addClass('ui-widget-content');
-    $(table).delegate('tr', 'hover', function() {
+    $('td', table).css('padding','5px 0 5px 8px');
+    /*$(table).delegate('tr', 'hover', function() {
         $('td', $(this)).toggleClass('ui-state-hover');
-    });
+    });*/
     /*$(table).delegate('tr', 'click', function() {
         $('td', $(this)).toggleClass('ui-state-highlight');
     });*/
