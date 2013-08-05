3125fc22f1995eb49dd2c1fda40442ce3db0ba5c
Christian Frichot <xntrik@gmail.com>
2013-08-05 16:22:03 +0800

    Updated mail config with generic settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'localhost',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
+  :user_name      => 'username',
+  :password       => 'password',
+  :domain         => 'domain.com'
 }
 ActionMailer::Base.delivery_method = :smtp
