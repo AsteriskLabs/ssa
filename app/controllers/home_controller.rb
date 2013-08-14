5116b55ea72889a0370fa33969b6d9361e062b54
Christian Frichot <xntrik@gmail.com>
2013-08-14 21:59:28 +0800

    Tidied up home controller - doesnt make unnecessary db calls anymore

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 class HomeController < ApplicationController
 	def index
-		if user_signed_in?
-			@user_status = {
-				:status => 1,
-				:email => current_user.email,
-				:logouturl => destroy_user_session_path
-			}.to_json
-		else 
-			@user_status = {
-				:status => 0,
-				:signupurl => new_user_registration_path,
-				:loginurl => new_user_session_path
-			}.to_json
-		end
+		# if user_signed_in?
+		# 	@user_status = {
+		# 		:status => 1,
+		# 		:email => current_user.email,
+		# 		:logouturl => destroy_user_session_path
+		# 	}.to_json
+		# else 
+		# 	@user_status = {
+		# 		:status => 0,
+		# 		:signupurl => new_user_registration_path,
+		# 		:loginurl => new_user_session_path
+		# 	}.to_json
+		# end
 	end
 end
