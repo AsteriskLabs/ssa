a5c7aa08f1cf892add8ed7a69b47b914e3573ac3
Christian Frichot <xntrik@gmail.com>
2013-08-14 11:55:48 +0800

    Removed default secret-token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ssa::Application.config.secret_token = '0eb74320cc7fc119db1ddec1822aa5d2edc5be134826261577624b59ce8e53d586d9507a415984c62ebdc21db0f388832a49a2d2c602721db927deb8f1b13a67'
